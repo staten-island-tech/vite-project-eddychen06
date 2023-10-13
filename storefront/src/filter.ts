6d16dec9bb26671be7085c9360e91dcd4209fed8
Eddy <57548065+eddychen06@users.noreply.github.com>
2023-10-13 10:13:00 -0400

    stuff

---
+++ storefront/src/filter.ts
@@ -0,0 +1,9 @@
+import { data } from "./data";
+
+function filter(brand) {
+    data.filter((brand) => {
+        return user.age > 40 && user.occupation === 'programmer';
+    });
+}
+
+
--- storefront/src/main.ts
+++ storefront/src/main.ts
@@ -1,30 +1,33 @@
 import './style.css'
-import { data } from './data.ts'
+import { data, carImages } from './data.ts'
 
-let carMake:string[] = []
+let carMake:string[] = data.map(car => car.carMake)
 
-for (const x in data) {
-    carMake.push(data[x]["carMake"])
-}
+carMake.filter((item, index) => carMake.indexOf(item) === index)
+    .forEach((brand:string) => {
+        document.querySelector<HTMLDivElement>("#brand")!.innerHTML += `
+            <p>${brand}</p>
+        `
 
-carMake = carMake.filter((item, 
-    index) => carMake.indexOf(item) === index).sort()
+    })
 
-for (const x in carMake) {
-    document.querySelector<HTMLDivElement>("#sidebar")!.innerHTML += `
-        <p>${carMake[x]}</p>
-    `
-}
+let bodyType:string[] = data.map(car => car.bodyType)
 
+bodyType.filter((item, index) => bodyType.indexOf(item) === index)
+    .forEach((type:string) => {
+        document.querySelector<HTMLDivElement>("#type")!.innerHTML += `
+            <p>${type}</p>
+        `
+
+    })
 
 for (const x in data) {
     let product = data[x]
 
-
-    document.querySelector<HTMLDivElement>('#app')!.innerHTML += `
+    document.querySelector<HTMLDivElement>('.product-list')!.innerHTML += `
     <div class="card">
-        <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_detail/menu/09_09/menu_asterion.png">
-        <h1>${product["carMake"]} ${product["carModel"]}</h1>
+        <img src=${carImages[x]["Link"]}>
+        <h2>${product["carMake"]} ${product["carModel"]}</h2>
         <p class="price">$${product["price"]}</p>
         <p>${product["colorOptions"]}</p>
     </div>
