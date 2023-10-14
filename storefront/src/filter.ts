3adde02134919063c3baf233af87aa3a78230abd
Eddy <eddie289k@gmail.com>
2023-10-14 19:50:44 -0400

    done

--- storefront/src/filter.ts
+++ storefront/src/filter.ts
@@ -1,9 +1,104 @@
-import { data } from "./data";
+import { data } from "./data"
 
-function filter(brand) {
-    data.filter((brand) => {
-        return user.age > 40 && user.occupation === 'programmer';
-    });
+let checkboxes = document.querySelectorAll<HTMLInputElement>('input[type="checkbox"]')
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+          addFilter(checkbox.value)
+        } else {
+          removeFilter(checkbox.value)
+        }
+      });
+});
+
+let filters:string[] = []
+
+function addFilter(filter:string) {
+  filters.push(filter)
+  return applyFilter()
+}
+
+function removeFilter(filter:string) {
+  filters = filters.filter(item => item !== filter)
+  return applyFilter()
 }
 
 
+function applyFilter() {
+
+  if (filters.length === 0) {
+    return updateDisplay(data)
+  }
+
+  let makes:string[] = []
+  let filterMakes:any[] = []
+  let types:string[] = []
+  let filterTypes:any[] = []
+
+
+  for (const x in filters) {
+    if (filters[x].includes("make")) {
+      makes.push(filters[x].replace('make ',''))
+    } else {
+      console.log(filters[x])
+      types.push(filters[x].replace('type ',''))
+    }
+  }
+
+  for (const x in makes) {
+    data.filter(car => {
+      if (car["carMake"].toUpperCase() == makes[x].toUpperCase()) {
+        filterMakes.push(car)
+      }
+    })
+  }
+
+  for (const x in types) {
+    data.filter(car => {
+      if (car["bodyType"].toUpperCase() == types[x].toUpperCase()) {
+        filterTypes.push(car)
+      }
+    })
+  }
+
+  if (makes.length > 0 && types.length === 0) {
+    updateDisplay(filterMakes)
+  } else if (makes.length === 0 && types.length > 0) {
+    updateDisplay(filterTypes)
+  } else if (makes.length > 0 && types.length > 0) {
+    let bothArr:any[] = []
+
+    for (const x in filterMakes) {
+      bothArr.push(filterMakes[x])
+    }
+
+    for (const x in filterTypes) {
+      bothArr.push(filterTypes[x])
+    }
+    
+    const sharedArr = bothArr.filter((item, index) => bothArr.indexOf(item) !== index); 
+    
+    updateDisplay(Array.from(new Set(sharedArr)))
+  } else {
+    return
+  }
+}
+
+function updateDisplay(cars:any[]) {
+  if (cars.length === 0) {
+    return document.querySelector<HTMLDivElement>('#product-list')!.innerHTML = `
+      <h1 id="error-msg">No cars found.</h1>
+    `
+  }
+  document.querySelector<HTMLDivElement>('#product-list')!.innerHTML = ``
+  for (const x in cars) {
+    document.querySelector<HTMLDivElement>('#product-list')!.innerHTML += `
+    <div class="product-card">
+        <img id="product-img" src="https://www.bentleymotors.com/content/dam/bentley/Master/Homestage%20Tiles/Bentayga%20Extended%20Wheelbase%20Azure_Bentayga%20EWB%20First%20Edition%20-%20Rear%203-4%201024x512.jpg/_jcr_content/renditions/original.image_file.700.350.file/Bentayga%20Extended%20Wheelbase%20Azure_Bentayga%20EWB%20First%20Edition%20-%20Rear%203-4%201024x512.jpg">
+        <h2>${cars[x]["carMake"]} ${cars[x]["carModel"]}</h2>
+        <p class="price">$${cars[x]["price"]}</p>
+    </div>
+    `
+  }
+}
--- storefront/src/main.ts
+++ storefront/src/main.ts
@@ -1,22 +1,25 @@
 import './style.css'
-import { data, carImages } from './data.ts'
+import { data } from './data.ts'
 
-let carMake:string[] = data.map(car => car.carMake)
+let carMake:string[] = data.map(car => car.carMake).sort()
 
 carMake.filter((item, index) => carMake.indexOf(item) === index)
     .forEach((brand:string) => {
         document.querySelector<HTMLDivElement>("#brand")!.innerHTML += `
-            <p>${brand}</p>
+            <label>
+                <input type="checkbox" class="filter-checkbox" value="make ${brand.toLowerCase()}"> ${brand}
+            </label>
         `
-
     })
 
 let bodyType:string[] = data.map(car => car.bodyType)
 
 bodyType.filter((item, index) => bodyType.indexOf(item) === index)
     .forEach((type:string) => {
-        document.querySelector<HTMLDivElement>("#type")!.innerHTML += `
-            <p>${type}</p>
+        document.querySelector<HTMLDivElement>("#body-type")!.innerHTML += `
+            <label>
+                <input type="checkbox" class="filter-checkbox" value="type ${type.toLowerCase()}"> ${type}
+            </label>
         `
 
     })
@@ -24,15 +27,23 @@ bodyType.filter((item, index) => bodyType.indexOf(item) === index)
 for (const x in data) {
     let product = data[x]
 
-    document.querySelector<HTMLDivElement>('.product-list')!.innerHTML += `
-    <div class="card">
-        <img src=${carImages[x]["Link"]}>
-        <h2>${product["carMake"]} ${product["carModel"]}</h2>
+    document.querySelector<HTMLDivElement>('#product-list')!.innerHTML += `
+    <div class="product-card">
+        <img id="product-img" src="https://www.bentleymotors.com/content/dam/bentley/Master/Homestage%20Tiles/Bentayga%20Extended%20Wheelbase%20Azure_Bentayga%20EWB%20First%20Edition%20-%20Rear%203-4%201024x512.jpg/_jcr_content/renditions/original.image_file.700.350.file/Bentayga%20Extended%20Wheelbase%20Azure_Bentayga%20EWB%20First%20Edition%20-%20Rear%203-4%201024x512.jpg">
+        <h2 class="product-name">${product["carMake"]} ${product["carModel"]}</h2>
         <p class="price">$${product["price"]}</p>
-        <p>${product["colorOptions"]}</p>
     </div>
     `
 } 
 
+let isDarkMode:boolean = false
 
-
+let button = document.getElementById("toggle-btn")?.addEventListener("click", function toggleDarkMode() {
+    if (isDarkMode === false) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        isDarkMode = true
+    } else {
+        document.documentElement.setAttribute("data-theme", "light")
+        isDarkMode = false
+    }
+})
